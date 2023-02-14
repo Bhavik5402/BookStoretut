9e10d5c630f8aec467e4138c489724c4bb339186
Bhavik Parmar <bjparmar.5402@gmail.com>
2023-02-14 14:25:30 +0530

    First Commit

--- BookStore/BookStore/Models/BookModel.cs
+++ BookStore/BookStore/Models/BookModel.cs
@@ -1,10 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace BookStore.Models
 {
     public class BookModel
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
 
         public string Title { get; set; }
 

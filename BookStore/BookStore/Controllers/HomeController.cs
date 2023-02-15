6298b77ca90eff880f077dc906a13fedd0e7de19
Bhavik Parmar <bjparmar.5402@gmail.com>
2023-02-15 15:24:34 +0530

    commited

--- BookStore/BookStore/Controllers/HomeController.cs
+++ BookStore/BookStore/Controllers/HomeController.cs
@@ -19,6 +19,12 @@ namespace BookStore.Controllers
             return View();
         }
 
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
+
         //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         //public IActionResult Error()
         //{
